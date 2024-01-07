1a74f1915189f1e8b1329009d5cc09e9085b8e85
garata <giorgio.arata@consultant.com>
2024-01-07 19:35:08 +0100

    Fix HTML characters script encoding

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ const Popup = () => html`
                 ${
                   (0 === index2 && questionVisible() > 0) ||
                   (index2 > 0 && index > index2)
-                    ? '<span class="filled"></span>'
+                    ? '🗸'
                     : index2 + 1
                 }</div>
                 <div class="popup__progress-line" ?visible=${
