f9a7cd18d1bfcac02a62f05c5a44e95b0f7daac8
garata <giorgio.arata@consultant.com>
2024-01-07 18:59:54 +0100

    Update 🗸 with &#128504; HTML entity

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ const Popup = () => html`
                 ${
                   (0 === index2 && questionVisible() > 0) ||
                   (index2 > 0 && index > index2)
-                    ? '🗸'
+                    ? '&#128504;'
                     : index2 + 1
                 }</div>
                 <div class="popup__progress-line" ?visible=${
